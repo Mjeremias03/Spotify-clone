7539208471b623a4db691dfb3001e790d6872b0b
jeremingolla <jeremingolla03@gmail.com>
2023-11-14 09:07:00 -0300

    Spotify-clone

---
+++ src/components/Saludo.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts"
+>
+const currentTime = new Date()
+const currentHorus = currentTime.getDate()
+let saludo = ""
+
+if(currentHorus < 12){
+    saludo = "Buenos Dias"
+}
+else if(currentHorus > 12 && currentHorus < 9){
+    saludo="Buenas Tardes"
+}else saludo = "Buenas noches"
+</script>
+
+<h1 class="text-3xl font-bold text-white">{saludo}</h1>
